9833016b25c933cf662abb13d25983912173e163
Yally-661 <yukiyada922@gmail.com>
2020-12-05 13:51:47 +0900

    Create setting.py

---
+++ setting.py
@@ -0,0 +1,8 @@
+TOKEN = "TOKEN" # bot token
+REACTION1 = "🆗" # positive reaction
+REACTION2 = "🆖" # negative reaction
+REACTION3 = "☑" # finish voting reaction
+REACTION1_TEXT = "参加"
+REACTION2_TEXT = "不参加"
+REACTION3_TEXT = "回答終了"
+TITLE = "投票"
